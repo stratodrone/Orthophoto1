d53da90cc0f07d971e4f29a12bd75f72c8c1bb31
stratodrone <107756055+stratodrone@users.noreply.github.com>
2022-10-11 20:40:22 -0400

    Neww

---
+++ webapp/data/RoofSurveyPricing_1.js
@@ -0,0 +1,8 @@
+var json_RoofSurveyPricing_1 = {
+"type": "FeatureCollection",
+"name": "RoofSurveyPricing_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
